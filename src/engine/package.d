cafceffd81c8c93f09c9c8e27660e64bf086aaf8
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-02-19 09:23:29 +0100

    GameObj renaming

--- src/engine/package.d
+++ src/engine/package.d
@@ -1,3 +1,36 @@
 module engine;
+import lib.memory.list;
 
-public import engine.gameobj;
+@safe pure:
+
+struct SceneObject {
+	void delegate() OnStart, OnUpdate;
+}
+
+struct Scene {
+	List!SceneObject objlist;
+
+	void Clear()
+	{
+		objlist.clear();
+	}
+	void Instantiate(SceneObject obj)
+	{
+		objlist.add(obj);
+	}
+}
+
+struct SceneLoader() {
+	Scene currentScene;
+	bool firstLoad = true;
+	void Load()(Scene scene)
+	{
+		if(firstLoad) {
+			firstLoad = false;
+		}
+		else {
+			currentScene.Clear();
+		}
+		currentScene = scene;
+	}
+}
