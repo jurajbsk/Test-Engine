1bfa7ff21b2ee43a923b1c6c48f8b95270d84c70
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-02-19 09:20:34 +0100

    Components

---
+++ src/engine/components/package.d
@@ -0,0 +1,7 @@
+module engine.parts;
+public import engine.parts.vector;
+
+struct Transform {
+	Vector2 pos;
+	alias pos this;
+}
---
+++ src/engine/components/vector.d
@@ -0,0 +1,14 @@
+module engine.parts.vector;
+
+struct Vector2 {
+	float x;
+	float y;
+}
+struct UVector2 {
+	uint x;
+	uint y;
+}
+struct Vector2_32 {
+	int x;
+	int y;
+}
