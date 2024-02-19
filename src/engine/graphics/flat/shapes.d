54016991e41d983d09805c545dceca4b7650da8c
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-02-19 09:23:54 +0100

    Square rendering

--- src/engine/graphics/flat/shapes.d
+++ src/engine/graphics/flat/shapes.d
@@ -19,4 +19,34 @@ struct Bitmap {
 	{
 		return pixels[x + width*y];
 	}
+
+	import lib.math;
+	void Square(Vector2_32 pos, uint side, Pixel colour = Pixel(), int outline = 0)
+	{
+		// TODO: Simplify breaking by first printing colour, then checking x outline for next pixel
+		uint d = min(side+pos.y, height-pos.y);
+		d = height;
+		with(pos)
+		for(int i = max(y, 0); i < min(side+y, height); i++)
+		{
+			bool yinside = cast(int)i+1 > outline+y && i < side-outline+y;
+			bool end;
+			int xstart = max(x, 0);
+			int xend = min(side+x, width);
+			for(int j = xstart; j < xend; j++)
+			{
+				// Empty space inside
+				bool xinside = j > outline-1+x;
+				if(outline && !end && yinside && xinside) {
+					j = xend;
+					if(j == width) break;
+					else j -= outline;
+					//if(x < 0) j += x + outline;
+					end = true;
+				}
+
+				this[j, i] = colour;
+			}
+		}
+	}
 }
