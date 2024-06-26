92e5147e6c459fa9d82445e8164e90df39931024
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-06-26 18:10:00 +0200

    Square fix

--- src/engine/graphics/flat/shapes.d
+++ src/engine/graphics/flat/shapes.d
@@ -12,41 +12,38 @@ struct Pixel {
 struct Bitmap {
 	Pixel[] pixels;
 	alias pixels this;
-	int width;
-	int height;
+	ushort width;
+	ushort height;
 
 	ref Pixel opIndex(size_t x, size_t y) nothrow pure
 	{
 		return pixels[x + width*y];
 	}
+}
 
-	import lib.math;
-	void Square(Vector2!int pos, uint side, Pixel colour = Pixel(), int outline = 0)
+import lib.math;
+void Square(ref Bitmap bits, Vec2!short pos, ushort side, Pixel colour = Pixel(), short outline = 0)
+{
+	// TODO: Simplify breaking by first printing colour, then checking x outline for next pixel
+	for(short i = max!short(pos.y, 0); i < min(side+pos.y, bits.height); i++)
 	{
-		// TODO: Simplify breaking by first printing colour, then checking x outline for next pixel
-		uint d = min(side+pos.y, height-pos.y);
-		d = height;
-		with(pos)
-		for(int i = max(y, 0); i < min(side+y, height); i++)
+		bool yinside = cast(short)i+1 > outline+pos.y && i < side-outline+pos.y;
+		bool end;
+		short xstart = max!short(pos.x, 0);
+		short xend = min!short(cast(short)(side+pos.x), cast(short)bits.width);
+		for(short j = xstart; j < xend; j++)
 		{
-			bool yinside = cast(int)i+1 > outline+y && i < side-outline+y;
-			bool end;
-			int xstart = max(x, 0);
-			int xend = min(side+x, width);
-			for(int j = xstart; j < xend; j++)
-			{
-				// Empty space inside
-				bool xinside = j > outline-1+x;
-				if(outline && !end && yinside && xinside) {
-					j = xend;
-					if(j == width) break;
-					else j -= outline;
-					//if(x < 0) j += x + outline;
-					end = true;
-				}
-
-				this[j, i] = colour;
+			// Empty space inside
+			bool xinside = j > outline-1+pos.x;
+			if(outline && !end && yinside && xinside) {
+				j = xend;
+				if(j == bits.width) break;
+				else j -= outline;
+				//if(x < 0) j += x + outline;
+				end = true;
 			}
+
+			bits[j, i] = colour;
 		}
 	}
 }
